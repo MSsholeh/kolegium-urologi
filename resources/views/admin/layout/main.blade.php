99eb08d7c7cb794d4aee2c881b7a1ebd5dec2db6
mssholeh <mssholeh@ymail.com>
2020-09-01 23:17:37 +0700

    update

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -320,6 +320,17 @@
 
 <!--end::Page Scripts -->
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-176287339-4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-176287339-4');
+</script>
+
+
 </body>
 
 <!-- end::Body -->
