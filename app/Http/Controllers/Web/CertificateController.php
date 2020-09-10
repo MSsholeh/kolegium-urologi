c000a53bfab949206195f4d0e3e0cb1052a8a35c
mssholeh <mssholeh@ymail.com>
2020-09-10 22:44:15 +0700

    update

--- app/Http/Controllers/Web/CertificateController.php
+++ app/Http/Controllers/Web/CertificateController.php
@@ -44,7 +44,7 @@ class CertificateController extends Controller
             return redirect()->back()->withErrors('Anda sudah pernah melakukan pengajuan sertifikat, silahkan melakukan pengajuan ulang!');
         }
 
-        if($check->status = "Request"){
+        if($check->status == "Request"){
             return redirect()->back()->withErrors('Anda memiliki pengajuan masih dalam proses, Anda belum bisa melakukan pengajuan sampai proses selesai');
         }
 
