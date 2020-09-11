c5d086c979388bb085271f2e900420643c8f6af9
mssholeh <mssholeh@ymail.com>
2020-09-11 11:52:15 +0700

    update

--- app/Http/Controllers/Admin/ResidentController.php
+++ app/Http/Controllers/Admin/ResidentController.php
@@ -42,7 +42,7 @@ class ResidentController extends Controller
     public function table(DataTables $datatables)
     {
         $query = Registrant::select('*', 'registrants.status as status_registrant', 'registrants.id as primary')
-            ->with('user', 'university', 'requirement.period');
+            ->with('user', 'university', 'requirement.period')->where('registrants.graduate', 'Lulus');
 
         $admin = Auth::user();
         if ($admin->isAdminUniversity()) {
